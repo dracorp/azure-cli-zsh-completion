1b5c87581c8bcff11dbbb4e000b2ab7b286257c9
Piotr Rogoża <rogoza.piotr@gmail.com>
2021-03-07 20:39:46 +0100

    Added notification is azure-cli is not installed

--- azure-cli-zsh-completion.plugin.zsh
+++ azure-cli-zsh-completion.plugin.zsh
@@ -1,6 +1,18 @@
 autoload -U +X bashcompinit && bashcompinit
 if [[ $(uname -s)  == Darwin ]]; then
-    source /usr/local/opt/azure-cli/etc/bash_completion.d/az
+    if [[ ! -r /usr/local/opt/azure-cli/etc/bash_completion.d/az ]]; then
+        echo 'azure-cli is not installed, you should "brew install azure-cli" first.'
+        echo 'See https://docs.microsoft.com/pl-pl/cli/azure/install-azure-cli-macos'
+        return 1
+    else
+        source /usr/local/opt/azure-cli/etc/bash_completion.d/az
+    fi
 else
-    source /etc/bash_completion.d/azure-cli
+    if [[ ! -r /etc/bash_completion.d/azure-cli ]]; then
+        echo 'azure-cli is not installed, you should "brew install azure-cli" first.'
+        echo 'See https://docs.microsoft.com/pl-pl/cli/azure/install-azure-cli'
+        return 1
+    else
+        source /etc/bash_completion.d/azure-cli
+    fi
 fi
