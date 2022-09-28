492e7807d7b7720f1bd2adc7837ae7c928791139
Rajasimman S <srajasimman@gmail.com>
2022-09-29 01:32:59 +0530

    added custome homebrew

--- azure-cli-zsh-completion.plugin.zsh
+++ azure-cli-zsh-completion.plugin.zsh
@@ -1,6 +1,8 @@
 autoload -U +X bashcompinit && bashcompinit
 if [[ $(uname -s)  == Darwin ]]; then
-    if [[ ! -r /usr/local/opt/azure-cli/etc/bash_completion.d/az ]]; then
+    if [[ -r ${HOMEBREW_CELLAR}/azure-cli/2.40.0/etc/bash_completion.d/az ]]; then
+        source ${HOMEBREW_CELLAR}/azure-cli/2.40.0/etc/bash_completion.d/az
+    elif [[ ! -r /usr/local/opt/azure-cli/etc/bash_completion.d/az ]]; then
         echo 'azure-cli is not installed, you should "brew install azure-cli" first.'
         echo 'See https://docs.microsoft.com/pl-pl/cli/azure/install-azure-cli-macos'
         return 1
